44cbaaee9b9db341ef7712c471fa81b9af197fef
Dogan Ucar <dogan@dogan-ucar.de>
2025-05-05 23:10:25 +0200

    status

---
+++ src/Http/Route.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace UcarSolutions\Entities\Http;
+
+enum Route: string
+{
+
+    case PATH = 'path';
+    case MIDDLEWARE = 'middleware';
+    case METHOD = 'method';
+    case NAME = 'name';
+    case OPTIONS = 'options';
+    case PATCH = 'patch';
+
+}
